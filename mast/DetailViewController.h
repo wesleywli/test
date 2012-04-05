1e9d0258a1005f461a98aead7192c6bd12bddc5a
wesley <wesley@wesleyteki-MacBook-Pro.local>
2012-04-05 13:55:02 +0800

    add new iphone project

---
+++ mast/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  mast
+//
+//  Created by wesley on 12/4/2.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
